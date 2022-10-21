4ea320e2880a9dffead593a0db3484c5331edd25
maliks1 <115067646+maliks1@users.noreply.github.com>
2022-10-21 07:26:21 -0700

    Update Tugas-3-2.cpp

--- Tugas-3-2.cpp
+++ Tugas-3-2.cpp
@@ -2,7 +2,6 @@
     2. Buatlah program untuk menghitung nilai akhr mahasiswa tersebut,
     jika diketahui nilai
     praktikum = 75, nilai UTS = 70, nilai UAS = 88!
-
     Malik Syafi'i
     1D-INFORMATIKA
 */
@@ -21,7 +20,7 @@ int main (){
     cout << "Nilai UTS = " << UTS << "\n";
     cout << "Nilai UAS = " << UAS << "\n";
     //proses hitung
-    NilaiAkhir = (praktikum * 20/100) + (uts * 30/100) + (uas * 50/100);
+    NilaiAkhir = (praktikum * 20/100) + (UTS * 30/100) + (UAS * 50/100);
     //output hasik
     cout << "Nilai akhirnya adalah = " << NilaiAkhir << "\n";
 
